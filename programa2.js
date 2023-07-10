1adee19e1f0e0864533c3574f9da3780d6731fe3
Nicholas Gerade <nicholasgerade@gmail.com>
2023-07-10 01:48:38 -0300

    Add. Arquivos

---
+++ programa2.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com Variáveis");
+// js é case sensitive
+
+const idade = 29;
+const nome = "Ricardo";
+
+console.log(idade, nome);
+
+// nao fazer
+//mes = "Janeiro"
---
+++ programa5.js
@@ -0,0 +1,18 @@
+console.log("Trabalhando com Atrb de Variáveis");
+
+let primeiroNome = "Nick"; // let = varia (pode ser modificado) 
+const sobrenome = "Gerade";
+
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNomeNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+
+console.log(nome);
+
+let contador = 0;
+contador = contador + 1;
+
+let idade;
+idade = 26;
+idade = idade + 1;
